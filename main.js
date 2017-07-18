c82a459c8f9ac088ed321ce6065360e27e43d333
Robert Howington <zerorob2787@gmail.com>
2017-07-17 22:31:31 -0400

    final commit without doing jagged border on header

--- main.js
+++ main.js
@@ -7,21 +7,29 @@ function reqListener (){
   let data = JSON.parse(this.responseText);
   let info ='';
   console.log(data);
+  document.querySelector(".header").innerHTML = `
+    <h1>${data.name}</h1>
+  `;
   let container = document.querySelector("main");
   container.innerHTML = `
+  <div class="card-container">
     <div class="basics">
       <h2>The Basics</h2>
       <ul>
         <li><span>Name:</span> ${data.name}</li>
-        <li><span>GitHub URL:</span> ${data.html_url}</li>
-        <li><span>Email:</span>${email}</li>
-        <li><span>Company:</span></li>
-        <li><span>Website:</span></li>
+        <li><span>GitHub URL:</span> <span class="list-ul">${data.html_url}</span></li>
+        <li><span>Email:</span> ${data.email}</li>
+        <li><span>Company:</span> ${data.scompany}</li>
+        <li><span>Website:</span> <span class="list-ul">${data.blog}</span></li>
       </ul>
     </div>
     <div class="story">
       <h2>The Story</h2>
-      <p></p>
+      <p>${data.bio}</p>
     </div>
+    <div class="avatar">
+      <img src="${data.avatar_url}">
+    </div>
+  </div>
     `
 }
